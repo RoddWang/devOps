a49415906ae17dc98f8c790244e888b74803ac3b
xiang-guan <xiang.guan@hpe.com>
2016-06-11 14:23:17 +0800

    testcase example
    Signed-off-by: xiang-guan <xiang.guan@hpe.com>

---
+++ src/test/java/com/hpe/it/sharedservice/devops/platform/controler/TestExample.java
@@ -0,0 +1,12 @@
+package com.hpe.it.sharedservice.devops.platform.controler;
+
+import org.junit.Test;
+
+public class TestExample {
+
+	@Test
+	public void test() {
+		System.out.println("Test success");
+	}
+
+}
